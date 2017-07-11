95d74c19a526af3097135df7b45a120df18203d5
Shia <rise.shia@gmail.com>
2017-07-11 19:35:40 +0900

    Extract chrome driver version

--- bin/travis_system_setup.sh
+++ bin/travis_system_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
+wget "http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
 rm chromedriver_linux64.zip
 mkdir /home/travis/bin
